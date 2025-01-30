38023085e9afb10ca1f8da7a4d3eb7c0d28796f9
OmKeshri <omkeshri21@gmail.com>
2025-01-30 23:58:35 +0530

    added authentication client side

--- Client/src/Components/Body/Body.jsx
+++ Client/src/Components/Body/Body.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import NavBar from "../Common/Navbar";
-import { Outlet } from "react-router-dom";
 import Footer from "../Common/Footer";
+import { Outlet, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../Utils/constants";
+import { addUser } from "../../Utils/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.response.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+      fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
--- srcBackend/middlewares/auth.js
+++ srcBackend/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Token not valid!");
+      return res.status(401).send("You are not logged in. Please login!")
     }
 
     const decodedObj = await jwt.verify(token, "Dev@Tinder#Om$1234%");
