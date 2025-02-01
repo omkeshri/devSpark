d2d406f224f343412a5bbc163da1f58f7441824d
OmKeshri <omkeshri21@gmail.com>
2025-02-01 17:34:32 +0530

    added accept reject request feature

--- Client/src/Components/Connections/ConnectionRequest.jsx
+++ Client/src/Components/Connections/ConnectionRequest.jsx
@@ -2,11 +2,12 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../../Utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../../Utils/connectionRequest";
+import { addRequests, removeRequests } from "../../Utils/connectionRequest";
 
 const ConnectionRequest = () => {
   const connectionRequest = useSelector((store) => store.connectionRequest);
   const dispatch = useDispatch();
+
   const fetchRequest = async () => {
     try {
       const request = await axios.get(BASE_URL + "/user/request/received", {
@@ -22,12 +23,27 @@ const ConnectionRequest = () => {
     fetchRequest();
   }, []);
 
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      console.log(res);
+      // fetchRequest();
+      dispatch(removeRequests(_id))
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (!connectionRequest) return;
   if (connectionRequest.length === 0) return <h1>No Connections Found!</h1>;
   return (
     <div className=" my-10">
       <h1 className="text-center font-bold text-4xl ">Connections Requests</h1>
-      <div className=" mx-10 my-10">
+      <div className=" mx-10 my-10 flex gap-5 flex-wrap">
         {connectionRequest.map((request) => {
           const { _id, firstName, lastName, photoUrl, age, gender, about } =
             request.fromUserId;
@@ -48,8 +64,18 @@ const ConnectionRequest = () => {
                 <h2 className="font-medium">{age + ", " + gender}</h2>
                 <h2 className="font-medium">{about}</h2>
                 <div className="flex justify-between mt-4">
-                  <button className="btn btn-primary">Accept</button>
-                  <button className="btn btn-secondary">Reject</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => reviewRequest("accepted", request._id)}
+                  >
+                    Accept
+                  </button>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => reviewRequest("rejected", request._id)}
+                  >
+                    Reject
+                  </button>
                 </div>
               </div>
             </div>
