0cd0cb3037416d725b080b59f2dc861f82a8f0fe
Om Keshri <119155465+omkeshri@users.noreply.github.com>
2025-02-02 02:05:57 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://omkeshri21:zE7cy0E4FJosuyLf@omkeshri.kyee5.mongodb.net/devTinder"
   );
 };
 
