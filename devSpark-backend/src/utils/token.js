0f6f1b27616304d4863fe33b8ed61ac776a692fc
Om Keshri <om.keshari@shyftlabs.io>
2025-11-29 15:09:37 +0530

    shift backend to individual repo

--- devSpark-backend/src/utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-export const createSession = async (id) => {
-    const token = await jwt.sign({ id: id }, process.env.JWT_PASSWORD, {
-        expiresIn: "1d",
-    });
-
-    return token;
-}
--- server/src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL);
-};
-
-module.exports = connectDB;
